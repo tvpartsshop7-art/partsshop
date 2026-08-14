656bbef455177bc909acb395ce97e048e7e937c8
qdigisoftware <qdigisoftware@gmail.com>
2026-08-14 10:35:22 +0530

    Configure Progressive Web App (PWA) manifest and service worker for mobile installation

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  // Let the browser fetch resources normally
+  e.respondWith(fetch(e.request));
+});
